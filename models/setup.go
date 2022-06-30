64c2a3a2305d5bd1f03e5cdd5d66e3c7f73999bd
quynt <quynguyenthe000@gmail.com>
2022-06-30 11:15:44 +0700

    add log config

--- models/setup.go
+++ models/setup.go
@@ -2,6 +2,10 @@ package models
 
 import (
 	"log"
+	"os"
+	"time"
+
+	"gorm.io/gorm/logger"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -9,8 +13,20 @@ import (
 
 var DB *gorm.DB
 
+var newLogger = logger.New(
+	log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+	logger.Config{
+		SlowThreshold:             time.Second,   // Slow SQL threshold
+		LogLevel:                  logger.Silent, // Log level
+		IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
+		Colorful:                  false,         // Disable color
+	},
+)
+
 func ConnectDatabase() {
-	database, err := gorm.Open(sqlite.Open("resume_builder.db"))
+	database, err := gorm.Open(sqlite.Open("resume_builder.db"), &gorm.Config{
+		Logger: newLogger,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
